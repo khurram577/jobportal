8f4ad2afca0f80aa38a19312ad7d266a06d635c7
khurramzates <howtod543@gmail.com>
2025-03-19 03:56:19 +0500

    Update registration.blade.php

--- resources/views/front/Account/registration.blade.php
+++ resources/views/front/Account/registration.blade.php
@@ -14,18 +14,22 @@
                         <div class="mb-3">
                             <label for="" class="mb-2">Name*</label>
                             <input type="text" name="name" id="name" class="form-control" placeholder="Enter Name" required>
+                            <p></p>
                         </div>
                         <div class="mb-3">
                             <label for="" class="mb-2">Email*</label>
                             <input type="text" name="email" id="email" class="form-control" placeholder="Enter Email">
+                            <p></p>
                         </div>
                         <div class="mb-3">
                             <label for="" class="mb-2">Password*</label>
                             <input type="password" name="Password" id="Password" class="form-control" placeholder="Enter Password">
+                            <p></p>
                         </div>
                         <div class="mb-3">
                             <label for="" class="mb-2">Confirm Password*</label>
                             <input type="password" name="Confirm_Password" id="Confirm Password" class="form-control" placeholder="Plz Confirm Password">
+                            <p></p>
                         </div>
                         <button class="btn btn-primary mt-2">Register</button>
                     </form>
@@ -49,6 +53,30 @@
          data: $("#registrationform").serializeArray(),
          dataType: 'json',
          success: function(response){
+            if (response.status == false) {
+                var errors - response.errors;
+                if (errors.name) {
+                    $("#name").addclass('is-invalid').siblings('p').addclass('invalid-feedback').html(errors.name);
+                } else {
+                    $("#name").removeClass('is-invalid').siblings('p').removeClass('invalid-feedback').html('');
+                }
+                if (errors.email) {
+                    $("#email").addclass('is-invalid').siblings('p').addclass('invalid-feedback').html(errors.email);
+                } else {
+                    $("#email").removeClass('is-invalid').siblings('p').removeClass('invalid-feedback').html('');
+                }
+                if (errors.password) {
+                    $("#password").addclass('is-invalid').siblings('p').addclass('invalid-feedback').html(errors.password);
+                } else {
+                    $("#password").removeClass('is-invalid').siblings('p').removeClass('invalid-feedback').html('');
+                }
+                if (errors.confirm_password) {
+                    $("#confirm_password").addclass('is-invalid').siblings('p').addclass('invalid-feedback').html(errors.confirm.password);
+                } else {
+                    $("#confirm_password").removeClass('is-invalid').siblings('p').removeClass('invalid-feedback').html('');
+                }
+            }
+                
 
          }
  });
