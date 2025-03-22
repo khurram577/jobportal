ae4a78cf0b1e3c2450c47eadc54f967ef81ba306
khurramzates <howtod543@gmail.com>
2025-03-22 20:22:35 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Accountcontroller;
 
 
 
+
 Route::get('/',[homecontroller::class,'index'])->name('home');
 Route::get('/account/register',[Accountcontroller::class,'registration'])->name('account.registration');
 Route::post('/account/process-register',[Accountcontroller::class,'processRegistration'])->name('account.processregistration');
