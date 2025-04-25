c40ee9f77fc17ca74694f889b3fe447f4ec5f647
khurramzates <howtod543@gmail.com>
2025-04-25 05:00:48 +0500

    h

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\homecontroller;
 use App\Http\Controllers\Accountcontroller;
-
-
-
+use Nette\Utils\Strings;
 
 Route::get('/',[homecontroller::class,'index'])->name('home');
 Route::get('/account/register',[Accountcontroller::class,'registration'])->name('account.registration');
 Route::post('/account/process-register',[Accountcontroller::class,'processRegistration'])->name('account.processregistration');
 
 
+Route::get('/test/{name?}', function (string $name) {
+    if ($name) {
+        return "<h1>". $name ."</h1>";
+    }else{
+
+        return "<h1>no test</h1>";
+    }
+})->where('name', '[a-zA-Z]+');
